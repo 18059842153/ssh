8c2b898367ab6ff2bd32f99b36e8d95f1def0c25
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-09-08 10:49:46 +0800

    sss

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,5 @@
 apt update
 apt install openssh
+mkdir /data/data/com.termux/files/home/.ssh
 cat ./id_rsa.pub >> /data/data/com.termux/files/home/.ssh/authorized_keys
 cat /data/data/com.termux/files/home/.ssh/authorized_keys
